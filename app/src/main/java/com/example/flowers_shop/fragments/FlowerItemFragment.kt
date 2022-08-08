7d02d547a13afd8bb63baadb12c6691e9ee5d038
JasMine <shonazmievajasmin@gmail.com>
2022-08-08 13:34:49 +0430

    Worked with layouts

---
+++ app/src/main/java/com/example/flowers_shop/fragments/FlowerItemFragment.kt
@@ -0,0 +1,45 @@
+package com.example.flowers_shop.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.flowers_shop.databinding.FragmentFlowerItemBinding
+
+class FlowerItemFragment : Fragment() {
+
+
+    private var _binding: FragmentFlowerItemBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFlowerItemBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
